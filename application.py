77baf82731730309039832c0a52c7fa3ea2efb75
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-11-29 17:51:16 -0500

    Add output digest to application.py

--- application.py
+++ application.py
@@ -6,7 +6,15 @@ with open("sample.json", mode="r", encoding="utf-8") as f:
     data = json.load(f)
 f.close()
 
+# Each list item will get printed as a separate line in the output file
+lines = []
+
 # Use sample-pretty.json to see the structure of the json if you need to
 # add new data to this lookup for loop.
 for i in data['items']:
-    print(f"{i['title']} - {i['origin']['title']}")
+    lines.append(f"{i['title']} - {i['origin']['title']}\n")
+
+# TODO - come up with a naming scheme so that digests are unique
+digest = open("digests/digest.md", mode="w", encoding="utf-8")
+digest.writelines(lines)
+digest.close()
