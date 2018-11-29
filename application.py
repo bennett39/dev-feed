0dea9d786a15441812e81c7084e371021ec4ce85
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-11-29 17:28:18 -0500

    Add comments to application.py

--- application.py
+++ application.py
@@ -1,8 +1,12 @@
 import json
 
+# TODO - still having touble with utf-8 encoding errors. Had to clean
+# sample.json in order to work with it for now. Long term, need a fix.
 with open("sample.json", mode="r", encoding="utf-8") as f:
     data = json.load(f)
 f.close()
 
+# Use sample-pretty.json to see the structure of the json if you need to
+# add new data to this lookup for loop.
 for i in data['items']:
     print(f"{i['title']} - {i['origin']['title']}")
