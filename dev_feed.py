f4de0000f67e5ace0c21af1d56e81aadb03146b0
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-11-29 09:28:01 -0500

    Test user_streamId

--- dev_feed.py
+++ dev_feed.py
@@ -4,24 +4,30 @@ from datetime import datetime, date
 from secrets import api_key
 
 # Main Feedly API. Use sandbox.feedly.com for testing.
-# https://developer.feedly.com/
+# Documentation at - https://developer.feedly.com/
 url_base = "https://cloud.feedly.com/v3/streams/contents"
+
+user_streamId = ("user/c04622d3-e092-4537-b5d5-a326858ffe1d/"
+                "category/Tech - Development")
 
 # Dictionary to feed into requests module to create URL
 # e.g. - "/v3/streams/contents?streamId=feed%2Fhttp..."
 payload = {
-    "streamId": "feed/http://feeds.engadget.com/weblogsinc/engadget/"
+    "streamId" = user_streamId
+    # Save this working payload
+    # "streamId": "feed/http://feeds.engadget.com/weblogsinc/engadget/"
 }
 
 # Get API response using requests module - requests must be installed
 r = requests.get(url_base, headers=api_key, params=payload)
 
 # Status message for user on command line
-message = f"URL: {r.url} \nStatus: {r.status_code}"
+message = f"URL: {r.url}\nStatus: {r.status_code}"
 print(message)
 
 # Store the results of the API call in a text file
-file_name = datetime.now().strftime('%M%S') + ".txt"
+file_name = ("output/" + datetime.now().strftime('%y-%m-%d-%H%M%S') + 
+            ".txt")
 f = open(file_name, "w")
 
 # If you need access to more information (e.g. r.text, r.encoding, etc),
@@ -29,5 +35,6 @@ f = open(file_name, "w")
 lines = [
     str(r.content),
 ]
+
 f.writelines(lines)
 f.close()
