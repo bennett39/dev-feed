66ce26f77c6fa1dc8deaf447b16ad9e40de0b5c7
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-04 13:47:19 -0500

    Simplify structure

--- dev_feed.py
+++ dev_feed.py
@@ -4,38 +4,27 @@ from secrets import api_key # Add your own api_key to secrets.py
 
 
 def main():
-    r = get_user_stream(api_key)
-    create_digest(r)
-
-
-def get_user_stream(api_key):
-    """
-    Fetches user article stream from Feedly API. Authentication
-    required
-    """
-    
     # Main Feedly API. Use sandbox.feedly.com for testing.
+    # Change endpoint to access other feeds/articles/users/etc
     # Documentation: https://developer.feedly.com/
-    url_base = "https://cloud.feedly.com/v3/streams/contents"
-
-    # Resource ID for the stream you want to access
-    # Documentation: https://developer.feedly.com/cloud/
-    user_streamId = ("user/c04622d3-e092-4537-b5d5-a326858ffe1d/"
-                    "category/Tech - Development")
+    endpoint = "https://cloud.feedly.com/v3/streams/contents"
 
     # Dictionary that feeds into requests module to create URL
     # e.g. - "/v3/streams/contents?streamId=feed%2Fhttp..."
-    # Optional: count (default 20), unreadOnly (boolean)
     # Documentation:
     # https://developer.feedly.com/v3/streams/#get-the-content-of-a-stream
     payload = {
-        "streamId": user_streamId,
+        # Resource ID for stream you want to access
+        "streamId": ("user/c04622d3-e092-4537-b5d5-a326858ffe1d/"
+                    "category/Tech - Development"),
+
+        # Optional settings
         "unreadOnly": True,
         "count": 5
     }
 
-    # Get API response using requests module - requests must be installed
-    return requests.get(url_base, headers=api_key, params=payload)
+    r = requests.get(endpoint, headers=api_key, params=payload)
+    create_digest(r)
 
 
 def create_digest(r):
